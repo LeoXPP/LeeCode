f383b1df32f725b681b9ce61e7eb169cd0f753ec
XIE <2010530759@qq.com>
2023-11-25 08:35:47 +0800

    第一次创建

---
+++ test1.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = nums1.size() - 1;
+        n--;
+        m--;
+        while(n>=0){
+            while(m>=0&&nums1[m]>nums2[n]){
+                swap(nums1[i--],nums1[m--]);
+            }
+            swap(nums1[i--],nums2[n--]);
+        }
+
+    }
+};
+
+
+
+
+
+int main( ){
+vector<int>  nums1={1,2,5,7,8,0,0,0};
+vector<int>  nums2={100,9,6,3,0};
+
+
+
+
+Solution test1;
+
+test1.merge(nums1,nums1.size(),nums2,nums2.size() );
+return 0;
+}
